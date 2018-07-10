790c92bddf639de4af77eac8ae4ef0df0174bd0d
Tailine <tailinechagas@gmail.com>
2018-07-09 22:28:54 -0300

    tweet functionality working

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
-
+// Event Listeners
 document.querySelector('#btn-quote').addEventListener('click', fetchQuote);
 document.addEventListener('DOMContentLoaded', fetchQuote);
+//document.querySelector('#twitter').addEventListener('click', shareQuote);
 
+// fetch the data from API
 async function fetchQuote() {
     try {
         const response = await fetch(`https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${Math.random()}`);
         const dataFetched = await response.json();
-        const [ data ] = dataFetched;
-        document.querySelector('blockquote').setAttribute('cite', `${data.link}`);
-        document.querySelector('blockquote').innerHTML= data.content;
-        document.getElementById('author').innerHTML = `- ${data.title}`;
+        displayQuote(dataFetched);
     } catch(err) {
         alert('Sorry, something went wrong.');
         location.reload();
     }
 }
 
-/*
-function getQuote() {
-    const xhr = new XMLHttpRequest();
-    //const randomNumber = Math.random();
-    xhr.open('GET', `https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=${Math.random()}`, true);
+function displayQuote(dataFetched) {
+    const [ data ] = dataFetched;  
+    const quoteArea = document.querySelector('blockquote');
+    const authorArea = document.getElementById('author');
+    
+    quoteArea.setAttribute('cite', `${data.link}`);
+    quoteArea.innerHTML = data.content;
+    authorArea.innerHTML = ` - ${data.title}`;
+    
+    setTwitterButton(quoteArea, authorArea);
+}
 
-    xhr.onload = function() {
-        if(this.status === 200) {
-            let quote = JSON.parse(this.responseText);
-            document.querySelector('blockquote').setAttribute('cite', `${quote[0].link}`);
-            document.querySelector('blockquote').innerHTML= quote[0].content;
-            document.getElementById('author').innerHTML = `- ${quote[0].title}`;
-        }
-    };
-    xhr.send();
-} */
+function setTwitterButton(q, a) {
+    const btnShare = document.querySelector('#twitter');
+    const quote = q.textContent;
+    const author = a.textContent;
+    
+    btnShare.setAttribute('href', `https://twitter.com/share?text=${quote}${author}`);
+}
+
+    
